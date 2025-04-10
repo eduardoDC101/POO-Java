544471d437cd5ccff750fce17f671d4f391f1ece
Eduardo Dourado <166930671+eduardoDC101@users.noreply.github.com>
2025-04-10 10:15:37 -0300

    Add files via upload

---
+++ Projeto_1/src/br/com/projeto_1/ctr/ClienteCTR.java
@@ -0,0 +1,65 @@
+
+package br.com.projeto_1.ctr;
+
+import java.sql.ResultSet;
+import br.com.projeto_1.dto.ClienteDTO;
+import br.com.projeto_1.dao.ClienteDAO;
+import br.com.projeto_1.dao.ConexaoDAO;
+public class ClienteCTR {
+    ClienteDAO clienteDAO = new ClienteDAO();
+    
+    public String inserirCliente(ClienteDTO clienteDTO){
+        try{
+            if(clienteDAO.inserirCliente(clienteDTO)){
+                return "Cliente Cadastrado com Sucesso!!";
+            }else{
+                return "Cliente NÂO Cadastrado.";
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            return "Cliente NÃO Cadastrado";
+        }
+    }
+    
+    public ResultSet consultarCliente(ClienteDTO clienteDTO, int opcao){
+        ResultSet rs = null;
+        rs = clienteDAO.consultarCliente(clienteDTO, opcao);
+        return rs;
+    }
+    
+    public void CloseDB(){
+        ConexaoDAO.CloseDB();
+    }
+    
+    public ClienteCTR(){
+        
+    }
+    
+    public String alterarCliente(ClienteDTO clienteDTO){
+        try{
+            if(clienteDAO.alterarCliente(clienteDTO)){
+                return "Cliente Alterado com Sucesso!!";
+            }else{
+                return "Cliente Não Alterado.";
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            return "Cliente NÃO Alterado";
+        }
+    }
+    
+    public String excluirCliente(ClienteDTO clienteDTO){
+        try{
+            if(clienteDAO.excluirCliente(clienteDTO)){
+                return "Cliente Excluído com Sucesso!!";
+            }else{
+                return "Cliente Não Excluído.";
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            return "Cliente NÃO Excluído";
+        }
+    }
+    
+    
+}
